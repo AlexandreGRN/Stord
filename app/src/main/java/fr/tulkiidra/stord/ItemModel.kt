43620d8c9170089b8b81b93f2076e64338276cec
AlexandreGRN <tulkiidra@gmail.com>
2023-04-29 19:59:17 +0200

    Add item model

---
+++ app/src/main/java/fr/tulkiidra/stord/ItemModel.kt
@@ -0,0 +1,9 @@
+package fr.tulkiidra.stord
+
+class ItemModel (
+    var parentCategory: String = "None",
+    var name: String = "Undefined",
+    var description: String = "Undefined",
+    var imageUrl: String = "https://cdn.pixabay.com/photo/2014/04/10/17/59/doubt-321144_960_720.png",
+    var favorite: Boolean = false
+)
