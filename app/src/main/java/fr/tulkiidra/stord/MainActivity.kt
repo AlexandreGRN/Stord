319779c701b779899ea64c92acf637f83cd51195
AlexandreGRN <tulkiidra@gmail.com>
2023-04-30 18:46:38 +0200

    When you don't have any object in favorite category, the application show you a specific page in the main category page

--- app/src/main/java/fr/tulkiidra/stord/MainActivity.kt
+++ app/src/main/java/fr/tulkiidra/stord/MainActivity.kt
@@ -2,9 +2,9 @@ package fr.tulkiidra.stord
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-//import fr.tulkiidra.stord.fragments.CategoryFragment
+import fr.tulkiidra.stord.fragments.CategoryFragment
 //import fr.tulkiidra.stord.fragments.FavoriteFragment
-import fr.tulkiidra.stord.fragments.ItemFragment
+//import fr.tulkiidra.stord.fragments.ItemFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceStrate: Bundle?){
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame_container, ItemFragment(this))
+        transaction.replace(R.id.frame_container, CategoryFragment(this))
         transaction.addToBackStack(null)
         transaction.commit()
     }
