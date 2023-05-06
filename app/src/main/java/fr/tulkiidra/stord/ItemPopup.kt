fd104ef526d83203777a6decda5c3757fe3e19fd
AlexandreGRN <tulkiidra@gmail.com>
2023-05-06 16:03:32 +0200

    Create every button in the item popup

--- app/src/main/java/fr/tulkiidra/stord/ItemPopup.kt
+++ app/src/main/java/fr/tulkiidra/stord/ItemPopup.kt
@@ -17,9 +17,44 @@ class ItemPopup(private val adapter: ItemCardAdapter, private val item: ItemMode
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.popup_item_detail)
 
+        /*  Create all the button/element in the popup  */
+
+        // Create all the responsive element (image and text)
         setupComponants()
+
+        // Create a button to close the popup
+        setupCloseButton()
+
+        // Create the button to delete an item
+        setupDeleteButton()
+
+        // Create a favorite button
+        setupFavoriteButton()
     }
 
+    private fun setupFavoriteButton(){
+        val favButton = findViewById<ImageView>(R.id.popup_favorite_item)
+
+        favButton.setOnClickListener{
+            item.favorite = !item.favorite
+            if (item.favorite) {
+                favButton.setImageResource(R.drawable.home)
+            } else {
+                favButton.setImageResource(R.drawable.star)
+            }
+        }
+    }
+    private fun setupCloseButton() {
+        findViewById<ImageView>(R.id.popup_close_button).setOnClickListener{
+            dismiss()
+        }
+    }
+
+    private  fun setupDeleteButton(){
+        findViewById<ImageView>(R.id.popup_supress_item).setOnClickListener{
+            dismiss()
+        }
+    }
     private fun setupComponants() {
         // Image
         val itemImage = findViewById<ImageView>(R.id.popup_item_image)
@@ -33,7 +68,16 @@ class ItemPopup(private val adapter: ItemCardAdapter, private val item: ItemMode
 
         // Remaining
         findViewById<TextView>(R.id.popup_item_remaining).text = item.remaining.toString()
+
         // Alert
         findViewById<TextView>(R.id.popup_item_alert).text = item.alert.toString()
+
+        // Favorite Logo
+        val favButton = findViewById<ImageView>(R.id.popup_favorite_item)
+        if (item.favorite) {
+            favButton.setImageResource(R.drawable.home)
+        } else {
+            favButton.setImageResource(R.drawable.star)
+        }
     }
 }
