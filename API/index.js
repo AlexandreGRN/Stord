e0a4a1638ae67651078108503604088463d273f3
AlexandreGRN <Tulkiidra@gmail.com>
2023-05-21 21:55:57 +0000

    item RESTFUL API complete

--- API/index.js
+++ API/index.js
@@ -32,7 +32,8 @@ app.get('/api/status', async (req, res) => {
     console.log("\{\"Status\": OK}");
 });
 
-//create a category
+//CREATE
+//category
 app.post('/api/create/category', async (req, res) => {
     jsonCategory = JSON.parse(JSON.stringify(req.body))
 
@@ -79,8 +80,61 @@ app.post('/api/create/category', async (req, res) => {
         console.log(err.message);
     }
 });
+//item
+app.post('/api/create/item', async (req, res) => {
+    jsonItem = JSON.parse(JSON.stringify(req.body))
 
-//get all categories
+    if (jsonItem.name){
+        var name = jsonItem.name
+    } else {
+        var name = "NULL"
+    }
+    if (jsonItem.description){
+        var description = jsonItem.description
+    } else {
+        var description = "NULL"
+    }
+    if (jsonItem.remaining){
+        var remaining = jsonItem.remaining
+    } else {
+        var remaining = "NULL"
+    }
+    if (jsonItem.alert ){
+        var alert  = jsonItem.alert 
+    } else {
+        var alert  = "NULL"
+    }
+    if (jsonItem.imageURL){
+        var imageURL = jsonItem.imageURL
+    } else {
+        var imageURL = "NULL"
+    }
+    if (jsonItem.favorite){
+        var favorite = jsonItem.favorite
+    } else {
+        var favorite = "NULL"
+    }
+    if (jsonItem.parent_category_id){
+        var parent_category_id = jsonItem.parent_category_id
+    } else {
+        var parent_category_id = "NULL"
+    }
+    var sql = 'INSERT INTO items(name, description, remaining, alert, imageURL, favorite, parent_category_id) VALUES '
+    sql = sql + `('${name}', '${description}', ${remaining}, ${alert}, '${imageURL}', ${favorite}, ${parent_category_id})`
+    try {
+        connection.query(sql, (err, results) =>{
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+    })
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+//GET ALL
+//categories
 app.get('/api/categories', async (req, res) => {
     const sql = 'SELECT * FROM categories';
 
@@ -90,11 +144,25 @@ app.get('/api/categories', async (req, res) => {
             return;
         }
         
-        console.log('Fetched users:', results);
+        console.log('Fetched categories:', results);
+    });
+});
+//items
+app.get('/api/items', async (req, res) => {
+    const sql = 'SELECT * FROM items';
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return;
+        }
+        
+        console.log('Fetched items:', results);
     });
 });
 
-//get a category
+//GET ONE
+//category
 app.get('/api/category/:id', async (req, res) => {
     try{
         var id = req.params.id
@@ -104,14 +172,31 @@ app.get('/api/category/:id', async (req, res) => {
                 console.error('Error executing query:', err);
                 return;
             }
-            console.log('Fetched user:', results);
+            console.log('Fetched category:', results);
+        });
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+//item
+app.get('/api/item/:id', async (req, res) => {
+    try{
+        var id = req.params.id
+        const sql = `SELECT * FROM items WHERE id = ${id}`;
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                return;
+            }
+            console.log('Fetched item:', results);
         });
     } catch (err) {
         console.log(err.message);
     }
 });
 
-//update a gategory
+//UPDATE
+//category
 app.put('/api/update/category/:id', async (req, res) => {
     try{
         jsonCategory = JSON.parse(JSON.stringify(req.body))
@@ -150,12 +235,78 @@ app.put('/api/update/category/:id', async (req, res) => {
         console.log(err.message);
     }
 });
-        
-//delete a category
+//item
+app.put('/api/update/item/:id', async (req, res) => {
+    try{
+        jsonItem = JSON.parse(JSON.stringify(req.body))
+        var changes = ""
+        if (jsonItem.name){
+            changes = changes + "name = " + "'" + jsonItem.name + "'" +  ", "
+        }
+        if (jsonItem.description){
+            changes = changes + "description = " + "'" +  jsonItem.description + "'" + ", "
+        }
+        if (jsonItem.remaining){
+            changes = changes + "remaining = " +  jsonItem.description + ", "
+        }
+        if (jsonItem.alert){
+            changes = changes + "alert = " +  jsonItem.description + ", "
+        }
+        if (jsonItem.imageURL){
+            changes = changes + "imageURL = " + "'" +  jsonItem.imageURL + "'" + ", "
+        }
+        if (jsonItem.favorite){
+            changes = changes + "favorite = " + jsonItem.favorite + ", "
+        }
+        if (jsonItem.parent_category_id){
+            changes = changes + "parent_category_id = " + jsonItem.parent_category_id + ", "
+        }
+        if (changes != ""){
+            changes = changes.slice(0, -2)
+        }
+        var id = req.params.id
+        var sql = 'UPDATE items SET ' + changes + ' WHERE id = ' + id
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                return;
+            }
+            console.log('Updated item:', results);
+        });
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+//DELETE
+//category
 app.delete('/api/delete/category/:id', async (req, res) => {
     try{
         const id = req.params.id
         sql = "DELETE FROM categories WHERE id = " + id
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                return;
+            }
+            console.log('Deleted category:', results);
+        });
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+//item
+app.delete('/api/delete/item/:id', async (req, res) => {
+    try{
+        const id = req.params.id
+        sql = "DELETE FROM items WHERE id = " + id
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                return;
+            }
+            console.log('Deleted item:', results);
+        });
     } catch (err) {
         console.log(err.message);
     }
