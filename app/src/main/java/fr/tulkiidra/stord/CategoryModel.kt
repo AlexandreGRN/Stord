1530cf63413d4e01246ea6dbbe0d186737b17f20
AlexandreGRN <tulkiidra@gmail.com>
2023-06-07 23:06:54 +0200

    Changing both object models

--- app/src/main/java/fr/tulkiidra/stord/CategoryModel.kt
+++ app/src/main/java/fr/tulkiidra/stord/CategoryModel.kt
@@ -1,8 +1,11 @@
 package fr.tulkiidra.stord
 
 class CategoryModel (
+    var id: Int = 123456789,
     var name: String = "Undefined",
     var description: String = "Undefined",
-    var imageUrl: String = "https://cdn.pixabay.com/photo/2014/04/10/17/59/doubt-321144_960_720.png",
-    var favorite: Boolean = false
+    var imageURL: String = "https://cdn.pixabay.com/photo/2014/04/10/17/59/doubt-321144_960_720.png",
+    var favorite: Boolean = false,
+    var owner_id: Int = 1,
+    var parent_category_id: Int = 0
 )
